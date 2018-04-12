29772eb13a63c9b5f31e2d7f5f7bc1594c4af5c2
poot0018 <poot0018@hz.nl>
2018-04-12 16:46:38 +0200

    full pizza

--- TestZooi/src/Main.java
+++ TestZooi/src/Main.java
@@ -3,23 +3,40 @@ public class Main {
 
     public static void main(String[] args){
 
-        class Pop
-        {
+//        int a;
+//
+//        if( 2 == 2 ) a = 2; else a = 5;
+//            System.out.println(a);
+//
+//        while ( a < 5 ) a++;
+//            System.out.println(a);
+//
+//        for ( a = 5; a < 100; a++);
+//        System.out.println(a);
 
-            int _corn;
+    }
 
-            public Pop(int corn)
-            {
-                _corn = corn;
-            }
+    public class Scoping {
 
-            public int Up()
-            {
-                return _corn;
-            }
+        private int x = 7;
+
+        private void Orange() {
+
+            int y = 0;
+            int z = x + y;
+
+            System.out.println(x);
+
         }
 
-        var machine = Pop(asdf);
+        public void Purple(){
+
+            int x = 0, y = 1;
+            int z = x + y;
+
+            System.out.println(x);
+
+        }
 
     }
 
